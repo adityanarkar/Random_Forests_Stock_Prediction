c9365c7773f41c1b1e5605f95fcfe59e5dd338b5
Aditya Narkar <aditya.narkar25@gmail.com>
2019-09-07 20:42:11 -0500

    Comparing two algos

--- paint.py
+++ paint.py
@@ -21,7 +21,7 @@ def drawOutput(stock, rfScore, zhScore, labels):
     width = 0.35  # the width of the bars
     fig, ax = plt.subplots()
     rects1 = ax.bar(x - width / 2, zhScore, width, label='Zero Hour')
-    # rects2 = ax.bar(x + width / 2, rfScore, width, label='Random Forests')
+    rects2 = ax.bar(x + width / 2, rfScore, width, label='Random Forests')
     ax.set_ylabel('Scores')
     ax.set_title(f"Scores for {stock}")
     ax.set_xticks(x)
@@ -29,7 +29,7 @@ def drawOutput(stock, rfScore, zhScore, labels):
     ax.legend()
 
     autolabel(ax, rects1)
-    # autolabel(rects2)
+    autolabel(ax, rects2)
 
     fig.tight_layout()
     plt.show()
@@ -42,20 +42,30 @@ def getZHResultsFromFile(filepath):
         for i in range(len(data)):
             score = data[i]["our_test_score"]
             results.append(score)
-        return results
+    return results
 
 
 def getRFResultsFromFile(filepath):
     results = []
+    with open(filepath, 'r') as f:
+        data = json.load(f)
+        for i in range(len(data)):
+            score = data[i]["model"]["our_test_score"]
+            results.append(score)
     return results
 
 
 def prepareData(rfResultsDir, zhResultsDir, listOfStocks):
     labels = range(10, 110, 10)
     for tickr in listOfStocks:
-        zhRes = getZHResultsFromFile(os.path.join(zhResultsDir, f"{tickr}.JSON"))
-        # rfRes = getRFResultsFromFile(os.path.join(zhResultsDir, f"{tickr}.JSON"))
-        drawOutput(tickr, 0, zhRes, labels)
+        zhFilePath = os.path.join(zhResultsDir, f"{tickr}.JSON")
+        rfFilePath = os.path.join(rfResultsDir, f"{tickr}.JSON")
+        if os.path.isfile(zhFilePath) and os.path.isfile(rfFilePath):
+            zhRes = getZHResultsFromFile(zhFilePath)
+            rfRes = getRFResultsFromFile(rfFilePath)
+            drawOutput(tickr, rfRes, zhRes, labels)
+        else:
+            print(f"File {zhFilePath} or {rfFilePath} does not exist.")
 
 
 stocks = []
@@ -64,4 +74,4 @@ with open("TICKR.txt", 'r') as f:
     for line in f.readlines():
         stocks.append(line.replace("\n",""))
 
-prepareData("", "./Results/ZH", stocks)
+prepareData("./Results/RF", "./Results/ZH", stocks)
