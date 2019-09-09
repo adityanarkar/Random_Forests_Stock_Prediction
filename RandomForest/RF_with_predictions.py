b88e123abb74f8498083e38a8d833f23769c54cb
Aditya Narkar <aditya.narkar25@gmail.com>
2019-09-09 02:08:45 -0500

    Feature selection

--- RandomForest/RF_with_predictions.py
+++ RandomForest/RF_with_predictions.py
@@ -1,8 +1,7 @@
-import collections
-
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFE
 
 
 def random_forest_classifier(data, n_estimators, max_depth):
@@ -12,7 +11,10 @@ def random_forest_classifier(data, n_estimators, max_depth):
     # training and testing
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     clf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
-    clf.fit(X_train, y_train)
-    score = clf.score(X_test, y_test)
+    selector = RFE(clf, 5, step=1)
+    selector = selector.fit(X_train, y_train)
+    score = selector.score(X_test, y_test)
+    print(selector.support_)
 
-    return clf, score
+    return selector, score
+
