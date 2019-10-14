6365fa664bf01065bb9e27a3a0df7e9b136f1218
Aditya Narkar <aditya.narkar25@gmail.com>
2019-10-14 17:45:36 -0500

    svm_fold -> clf

--- SVM/svm_fold.py
+++ SVM/svm_fold.py
@@ -22,6 +22,7 @@ def svm_classifier(data, features_to_select, C, future_day):
 
     # training and testing
     train_indices, test_indices = k_splits.get_max_k_splits(X, k=10, size_of_each_split=future_day)
+    clf = 0
     for train_index, test_index in zip(train_indices, test_indices):
         X_train, y_train, X_test, y_test = k_splits.get_train_test_set(X, y, train_index, test_index)
         clf = LinearSVC(random_state=0, tol=1e-5, C=C, max_iter=10000)
