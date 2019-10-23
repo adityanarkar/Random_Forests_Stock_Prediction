68e28093c6512bfd9859bed157457b7c6faaf453
Aditya Narkar <aditya.narkar25@gmail.com>
2019-10-22 19:40:56 -0500

    SVM for FS with all kernels

--- SVM/svm.py
+++ SVM/svm.py
@@ -1,10 +1,9 @@
 import numpy as np
-from sklearn import svm
 from sklearn.exceptions import ConvergenceWarning
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC
 from sklearn.feature_selection import RFE
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
 from sklearn.utils.testing import ignore_warnings
 
 
@@ -15,15 +14,15 @@ def scale_data(X):
 
 
 @ignore_warnings(category=ConvergenceWarning)
-def svm_classifier(data, features_to_select, C):
+def svm_classifier(data, features_to_select, C, kernel, degree):
     X = np.asarray(list(map(lambda row: row[:-1], data)))
     X = scale_data(X)
     y = np.asarray(list(map(lambda row: row[-1], data)))
 
     # training and testing
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    clf = LinearSVC(random_state=0, tol=1e-5, C=C, max_iter=10000)
-    clf = RFE(clf, 25, step=1)
+    clf = SVC(random_state=0, tol=1e-5, C=C, max_iter=10000, kernel=kernel, degree=degree)
+    clf = RFE(clf, features_to_select, step=1)
     clf.fit(X_train, y_train)
     score = clf.score(X_test, y_test)
     print(score)
